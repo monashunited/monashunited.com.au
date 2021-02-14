befd66dbcbbf2f73578b5606af3578ef88ecd19f
wildone <wildone@users.noreply.github.com>
2021-02-14 13:36:51 +0000

    deploy: 26bac9fd304432e85dec71830c0bf0fdd0a6f131

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,15 +5,21 @@ var store = [{
         "url": "/blog/2020/12/22/monash-council-serves-xmas-ace",
         "teaser": null
       },{
-        "title": "Monash United",
-        "excerpt":"Welcome to Monash United! This community group exist to unite all of the community groups to give us a single powerful voice! We Need Your Help! Keep Monash Council Accountable! Monash Councillors have done a secret deal to build a significant sporting infrastructure east of Melbourne, a 26mil Tennis Complex...","categories": ["blog"],
+        "title": "Welcome Monash United",
+        "excerpt":"  Our Purpouse   This community group exist to unite all of the community groups to give us a single powerful voice!   We Need Your Help!   Keep Monash Council Accountable!   Monash Councillors have done a secret deal to build a significant sporting infrastructure east of Melbourne, a 26mil Tennis Complex located on Glen Waverley Golf Course … at your expense!   If you are …      A ratepayer,   a golfer,   a tennis player,   a lover of nature,   or care about mental health services, help us find a better way.   Our rates will go up, we will lose trees and the open spaces we love, local tennis clubs will close, and traffic will increase.   Show your support by joining our group and stay up to date with what’s happening.  ","categories": ["blog"],
         "tags": ["blog"],
-        "url": "/blog/2021/02/01/monash-united",
+        "url": "/blog/2021/02/01/welcome",
         "teaser": null
       },{
-        "title": "Monash United Flyer Drop",
-        "excerpt":"  Monash United in your Mail Box   This community of volunteers have been busy delivering vital information to you.   If we have not got to you, you can download it here:   Monash United Flyer  ","categories": ["blog"],
+        "title": "New Flyer",
+        "excerpt":"  Monash United Flyer                                                                                                                                                                                                                                                ❚❚             ","categories": ["blog"],
         "tags": ["blog"],
-        "url": "/blog/2021/02/14/monash-united-flyer-drop",
+        "url": "/blog/2021/02/12/new-flyer",
+        "teaser": null
+      },{
+        "title": "Flyer Drop",
+        "excerpt":"  Flyer Drop   This community of volunteers have been busy delivering vital information to you.   If we have not got to you, you can download it here:   Monash United Flyer  ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/blog/2021/02/14/flyer-drop",
         "teaser": null
       }]
