e58c17665a3965b1e13a27ccbc494217fc865472
wildone <max.barrass@gmail.com>
2021-02-14 22:45:07 +1100

    update scripts

--- start.ps1
+++ start.ps1
@@ -4,8 +4,11 @@ $SITE_NAME = Get-Content .\CNAME -Raw
 $SITE_NAME = $SITE_NAME -replace "[.\n\r]", "-"
 $CONATINER_NAME = "build-$SITE_NAME"
 
+$PORT = Get-Content .\dev.port -Raw
+$PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
+
 #start docker build container
-docker run --name $CONATINER_NAME -d -p 8105:8100 -p 35728:35729 -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack sleep inf
+docker run --name $CONATINER_NAME -d -p $PORT:$PORT -p $PORT_LIVERELOAD:$PORT_LIVERELOAD -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack sleep inf
 
 #connect to container
 docker exec -it $CONATINER_NAME bash --login
