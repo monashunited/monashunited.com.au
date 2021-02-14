297bdc39362ba44456ed7780481a37ddfb5d037b
wildone <wildone@users.noreply.github.com>
2021-02-14 06:54:43 +0000

    deploy: 22d6a0c03f544f0c750daf38158b7a7b772d81af

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
         "title": "Monash Serves Xmas Ace!",
-        "excerpt":"  Xmas Surprise   A few days before Xmas, a knock on the door delivered news that would ruin xmas for everyone in GWav.   We were told that we needed more sporting facilities and we were going to get them at expense of everyone.   ","categories": ["blog"],
+        "excerpt":"  Xmas Surprise   A few days before Xmas, a knock on the door delivered the news that would ruin Xmas for everyone in GWav.   We were told that we needed more sporting facilities and we were going to get them at the expense of everyone.   ","categories": ["blog"],
         "tags": ["blog"],
         "url": "/blog/2020/12/22/monash-council-serves-xmas-ace",
         "teaser": null
       },{
         "title": "Monash United",
-        "excerpt":"Welcome to Monash United! This community group exist to unite all of the community groups to give us single powerfull voice! We Need Your Help! Keep Monash Council Accountable! Monash Councillors have done a secret deal to build a significant sporting infrastructure east of Melbourne, a 26mil Tennis Complex located...","categories": ["blog"],
+        "excerpt":"Welcome to Monash United! This community group exist to unite all of the community groups to give us a single powerful voice! We Need Your Help! Keep Monash Council Accountable! Monash Councillors have done a secret deal to build a significant sporting infrastructure east of Melbourne, a 26mil Tennis Complex...","categories": ["blog"],
         "tags": ["blog"],
         "url": "/blog/2021/02/01/monash-united",
+        "teaser": null
+      },{
+        "title": "Monash United Flyer Drop",
+        "excerpt":"Monash United in your Mail Box This community of voloniteers We Need Your Help! Keep Monash Council Accountable! Monash Councillors have done a secret deal to build a significant sporting infrastructure east of Melbourne, a 26mil Tennis Complex located on Glen Waverley Golf Course … at your expense! If you...","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/blog/2021/02/14/monash-united-flyer-drop",
         "teaser": null
       }]
