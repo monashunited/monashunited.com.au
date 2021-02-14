4f2d9a55a592350ca4fa1d67ae16871b018a2831
wildone <wildone@users.noreply.github.com>
2021-02-14 06:39:43 +0000

    deploy: fe5cd57c01aae8e02f8654d6cc2370c18988c035

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,12 @@
 var store = [{
+        "title": "Monash Serves Xmas Ace!",
+        "excerpt":"  Xmas Surprise   A few days before Xmas, a knock on the door delivered news that would ruin xmas for everyone in GWav.   We were told that we needed more sporting facilities and we were going to get them at expense of everyone.   ","categories": ["blog"],
+        "tags": ["blog"],
+        "url": "/blog/2020/12/22/monash-council-serves-xmas-ace",
+        "teaser": null
+      },{
         "title": "Monash United",
-        "excerpt":"Welcome to Monash United!  ","categories": ["blog"],
+        "excerpt":"Welcome to Monash United! This community group exist to unite all of the community groups to give us single powerfull voice! We Need Your Help! Keep Monash Council Accountable! Monash Councillors have done a secret deal to build a significant sporting infrastructure east of Melbourne, a 26mil Tennis Complex located...","categories": ["blog"],
         "tags": ["blog"],
         "url": "/blog/2021/02/01/monash-united",
         "teaser": null
