1ccec3ba0522c2a097cd4228e37cc8b7abc0abb9
wildone <wildone@users.noreply.github.com>
2021-02-16 05:48:20 +0000

    deploy: 46740f3fe90ca4ef6c4ce8c017cbd9338cc79de7

--- start.ps1
+++ start.ps1
@@ -8,7 +8,7 @@ $PORT = Get-Content .\dev.port -Raw
 $PORT_LIVERELOAD = Get-Content .\dev-livereload.port -Raw
 
 #start docker build container
-docker run --name $CONATINER_NAME -d -p $PORT:$PORT -p $PORT_LIVERELOAD:$PORT_LIVERELOAD -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack sleep inf
+docker run --name $CONATINER_NAME -d -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD} -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack sleep inf
 
 #connect to container
 docker exec -it $CONATINER_NAME bash --login
