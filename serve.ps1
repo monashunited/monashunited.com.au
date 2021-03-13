04fa208021090d5673b7813c936ed1378a8de7ec
wildone <max.barrass@gmail.com>
2021-03-13 12:53:33 +1100

    fix params [skip ci]

--- serve.ps1
+++ serve.ps1
@@ -15,5 +15,5 @@ if ($DOCKER_INSPECT_CHECK -contains "running") {
   docker exec -it $CONATINER_NAME bash
 } else {
   Write-Host "Starting Docker container to run build server:"
-  docker run --name $CONATINER_NAME -it --rm -p $PORT:$PORT -p $PORT_LIVERELOAD :$PORT_LIVERELOAD  -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
+  docker run --name $CONATINER_NAME -it --rm -p ${PORT}:${PORT} -p ${PORT_LIVERELOAD}:${PORT_LIVERELOAD}  -v ${PWD}:/build/source:rw aemdesign/centos-java-buildpack bash --login /build/source/docker-serve.sh
 }
